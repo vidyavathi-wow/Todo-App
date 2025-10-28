41b3f4b5d9b3e345c420125eaac2e5a4d65ea886
vidyavathi-wow <vidyavathi.telugu@wowlaz.com>
2025-10-28 14:19:35 +0530

    chore: prepare backend for deployment

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,30 @@
-require("dotenv").config()
-const { Sequelize } = require("sequelize");
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect:"postgres",
-    port: Number(process.env.DB_PORT) || 5432,
-    logging: false,
-  }
-);
+const isProduction = process.env.NODE_ENV === 'production';
+
+const sequelize = isProduction
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      logging: false,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        logging: false,
+      }
+    );
 
 module.exports = sequelize;
