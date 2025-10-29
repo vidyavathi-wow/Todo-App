961830cc67b81225e9d7e7ec2d5eaba62ecc5ccb
vidyavathi-wow <vidyavathi.telugu@wowlaz.com>
2025-10-29 11:04:56 +0530

    fix: sequelize fix

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -5,6 +5,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
