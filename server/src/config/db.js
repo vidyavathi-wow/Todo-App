30633bb2fa7354d8affeb744c7b2b39df5604d34
vidyavathi-wow <vidyavathi.telugu@wowlaz.com>
2025-10-29 11:00:45 +0530

    fix: db fix

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,16 +1,10 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    port: Number(process.env.DB_PORT) || 5432,
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+});
 
 module.exports = sequelize;
